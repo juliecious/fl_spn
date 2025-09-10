7f476bf8924c92d921d13f52ffbf471713a4c091
Julie Fang <julie.fang@tum.de>
2025-09-10 10:56:16 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     author_email="",
     url="https://github.com/juliecious/fl_spn",
     packages=find_packages(
-        exclude=["*.ipynb", "*.ipynb_checkpoints", "tests*"]
+        include=["fl_spn", "fl_spn.*"],
+        exclude=["*.ipynb", "*.ipynb_checkpoints", "tests*"],
     ),  # 自動尋找子模組
     install_requires=[
         "simple-einet @ git+https://github.com/juliecious/simple-einet.git@20a4cb4f905d9f100d41184e4c107107b5d6899f",
