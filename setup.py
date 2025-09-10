9aa730c08231d1104f1a0441196706e2f0fd379c
Julie Fang <julie.fang@tum.de>
2025-09-10 10:54:29 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -11,17 +11,7 @@ setup(
         exclude=["*.ipynb", "*.ipynb_checkpoints", "tests*"]
     ),  # 自動尋找子模組
     install_requires=[
-        "numpy==1.26.4",
-        "pandas==2.3.2",
-        "pre-commit",
         "simple-einet @ git+https://github.com/juliecious/simple-einet.git@20a4cb4f905d9f100d41184e4c107107b5d6899f",
-        "torch==2.8.0",
-        "tqdm",
-        "plotly",
-        "matplotlib",
-        "seaborn",
-        "scikit-learn==1.7.1",
-        "scipy==1.14.1",
     ],
     python_requires=">=3.10",
     include_package_data=True,
